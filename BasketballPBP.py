8a8b1ed6bc405b5359c9a4f929c0190bb17f1a7a
martinfinleyj <martin.finleyj@gmail.com>
2026-09-15 17:19:54 -0500

    curry model
    created a model that predicts stephen curry percentage to make a shot based on features like: x,y,dist,subtype,etc.
    
    - removed dropdown
    - focusing on steph curry for now
       - will expand

--- BasketballPBP.py
+++ BasketballPBP.py
@@ -11,6 +11,69 @@ Created on Sun Sep 13 21:19:45 2026
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import seaborn as sns
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+# mapping player names in "pbp2025.csv" to full names
+pbp_25 = pd.read_csv("pbp2025.csv")
+player_map = pd.read_csv("PlayerIndex_nba_stats.csv")
+player_map = player_map.loc[:, ["PERSON_ID", "PLAYER_LAST_NAME", "PLAYER_FIRST_NAME"]]
+player_map = player_map.rename(columns={"PERSON_ID": "playerid"})
+
+result = pd.merge(pbp_25, player_map, on = "playerid")
+result["Player Name"] = result["PLAYER_FIRST_NAME"] + " " + result["PLAYER_LAST_NAME"]
+
+# clean data and filter by actual shots
+shots = result.loc[:, ["playerid", "Player Name","x", "y","dist", "type", "subtype", "desc"]]
+shots = shots[shots["type"].isin(["Made Shot", "Missed Shot"])]
+
+# specify 3pt shots
+shots["made"] = (shots["type"] == "Made Shot").astype('int')
+shots["3PT"] = np.where(shots["desc"].str.contains("3PT"), True, False)
+
+shots['court_x'] = shots['x']/10 + 25
+shots['court_y'] = shots['y']/10 + 5.25
+
+area_rect = patches.Rectangle(
+    (17, 0), 16, 19,
+    fill=False,
+    linewidth=2
+)
+
+rx, ry = area_rect.get_xy()  # Bottom-left corner coordinate
+rw = area_rect.get_width()
+rh = area_rect.get_height()
+
+# 3. Vectorized boundary check using your DataFrame columns
+in_x = (shots["court_x"] >= rx) & (shots["court_x"] <= rx + rw)
+in_y = (shots["court_y"] >= ry) & (shots["court_y"] <= ry + rh)
+
+
+# 4. Save the true/false mask to your DataFrame
+shots["inside_paint"] = in_x & in_y
+paint_total = len(shots[shots['inside_paint'] == True])
+paint_made = len(shots[(shots['made'] == 1) & (shots["inside_paint"] == True)])
+paint_percent_league = round((paint_made / paint_total) * 100, 1)
+
+three_total = len(shots[shots['3PT'] == 1])
+three_made = len(shots[(shots['made'] == 1) & (shots["3PT"] == True)])
+three_pct_league = round((three_made / three_total) * 100, 1)
+
+shots["midrange"] = (shots["inside_paint"] == False) & (shots["3PT"] == 0)
+
+mid_total = len(shots[shots['midrange'] == True])
+mid_made = len(shots[(shots['made'] == 1) & (shots["midrange"] == True)])
+mid_pct_league = round((mid_made / mid_total) * 100, 1)
+
+shots_ordered = shots.reindex(columns=["playerid", "Player Name", "x", "y", "court_x", 
+                                       "court_y", "dist", "3PT", "inside_paint",
+                                       "midrange", "subtype", "desc","made"])
+
+shots_player = shots_ordered[shots_ordered["Player Name"] == "Stephen Curry"].copy()
+
+
+
 
 fig, ax = plt.subplots(figsize=(10,8))
 
@@ -90,12 +153,6 @@ ax.add_patch(
 
 
 #paint
-
-area_rect = patches.Rectangle(
-    (17, 0), 16, 19,
-    fill=False,
-    linewidth=2
-)
 ax.add_patch(
     area_rect
 )
@@ -151,46 +208,45 @@ ax.set_ylim(0, 47)
 
 
 
-plt.title(f"{name} 2024-2025 Shot Distribution", loc = 'center')
+plt.title("Stephen Curry 2024-2025 Shot Distribution", loc = 'center')
 plt.axis('off')
 
 # 2 shot percent made within an area made
 
-rx, ry = area_rect.get_xy()  # Bottom-left corner coordinate
-rw = area_rect.get_width()
-rh = area_rect.get_height()
 
-# 3. Vectorized boundary check using your DataFrame columns
-in_x = (shots_player["court_x"] >= rx) & (shots_player["court_x"] <= rx + rw)
-in_y = (shots_player["court_y"] >= ry) & (shots_player["court_y"] <= ry + rh)
-
-
-# 4. Save the true/false mask to your DataFrame
-shots_player["inside_paint"] = in_x & in_y
-paint_total = len(shots_player[shots_player['inside_paint'] == True])
-paint_made = len(shots_player[(shots_player['made'] == 1) & (shots_player["inside_paint"] == True)])
-paint_percent = round((paint_made / paint_total) * 100, 1)
-
-three_total = len(shots_player[shots_player['3PT'] == 1])
-three_made = len(shots_player[shots_player['3PT_made'] == 1])
-three_pct = round((three_made / three_total) * 100, 1)
-
-shots_player["midrange"] = (shots_player["inside_paint"] == False) & (shots_player["3PT"] == 0)
-
-mid_total = len(shots_player[shots_player['midrange'] == True])
-mid_made = len(shots_player[(shots_player['made'] == 1) & (shots_player["midrange"] == True)])
-mid_pct = round((mid_made / mid_total) * 100, 1)
-
-plt.text(25, 8, f"PAINT {paint_percent}% | {paint_made}/{paint_total}", backgroundcolor = "black", color = "white")
-plt.text(36, 30, f"THREE {three_pct}% | {three_made}/{three_total}", backgroundcolor = "black", color = "white")
-plt.text(5, 15, f"MIDRANGE {mid_pct}% | {mid_made}/{mid_total}", backgroundcolor = "black", color = "white")
+plt.text(25, 8, f"PAINT {paint_percent_league}% | {paint_made}/{paint_total}", backgroundcolor = "black", color = "white")
+plt.text(36, 30, f"THREE {three_pct_league}% | {three_made}/{three_total}", backgroundcolor = "black", color = "white")
+plt.text(5, 15, f"MIDRANGE {mid_pct_league}% | {mid_made}/{mid_total}", backgroundcolor = "black", color = "white")
 ax.set_aspect('equal')
 plt.show()
 
+train_df = shots_ordered[shots_ordered["Player Name"] != "Stephen Curry"]
 
-# 3 better graphics
-# total averages for all players
-# compare 2 players (seperate graphs or overlap)
-# 4 get all years csvs- put into one large clean dataset and be able to pick year and player
-# expected value for shot locations
-# possibly in the future be able to move small area and it detects percentage and shots made in the area
+features = ["court_x",
+              "court_y",
+              "dist",
+              "3PT",
+              "inside_paint",
+              "midrange",
+              "subtype"]
+
+X = train_df[features]
+X = pd.get_dummies(X, columns=['subtype'], dtype=int).copy()
+X_curry = shots_player[features]
+X_curry = pd.get_dummies(X_curry, columns=['subtype'], dtype=int).copy()
+
+y = train_df["made"]
+
+X_curry = X_curry.reindex(columns=X.columns, fill_value=0)
+
+
+model = LogisticRegression(max_iter = 1000)
+model.fit(X, y)
+
+shots_player["expected_prob"] = model.predict_proba(X_curry)[:, 1]
+
+expected_meanpct = shots_player["expected_prob"].mean()
+print(expected_meanpct)
+
+# since curry shot 40.8% from the field 24-25, that means that curry shoots 4% 
+# above the field compared to the predicted percentage
